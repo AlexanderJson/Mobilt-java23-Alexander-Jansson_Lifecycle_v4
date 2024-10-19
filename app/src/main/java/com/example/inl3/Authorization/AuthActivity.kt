e996eff0c9980962af6eebd0cf910a35d316c0a1
Alexander Jansson <alexanderjson@hotmail.com>
2024-10-19 11:07:39 +0200

    added login frag

--- app/src/main/java/com/example/inl3/Authorization/AuthActivity.kt
+++ app/src/main/java/com/example/inl3/Authorization/AuthActivity.kt
@@ -1,4 +1,24 @@
 package com.example.inl3.Authorization
 
-class AuthActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.inl3.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_auth)
+
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.auth_container, LoginFragment())
+                .commit()
+
+        }
+
+    }
 }
--- app/src/main/java/com/example/inl3/Authorization/LoginFragment.kt
+++ app/src/main/java/com/example/inl3/Authorization/LoginFragment.kt
@@ -1,4 +1,44 @@
 package com.example.inl3.Authorization
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.inl3.R
+
+ class LoginFragment : Fragment() {
+
+     override fun onCreateView(
+         inflater: LayoutInflater,
+         container: ViewGroup?,
+         savedInstanceState: Bundle?
+     ): View? {
+
+         val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val usernameInput = view.findViewById<EditText>(R.id.username)
+         val passwordInput = view.findViewById<EditText>(R.id.password)
+         val loginBtn = view.findViewById<Button>(R.id.loginBtn)
+
+         loginBtn.setOnClickListener {
+             // fil auth method
+         }
+
+
+         return view
+
+     }
+     public fun checkAuth(usernameInput: String, passwordInput: String): Boolean {
+         return false
+     }
+
+     public fun authSuccess() {}
+
+     public fun authFailure(){}
+
+     public fun checkDB(){}
 }
